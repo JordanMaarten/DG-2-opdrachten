d9270c359feacbdd4e772a676caf7542c07f478b
JeffreyMeijer <123974@student.drenthecollege.nl>
2019-04-08 11:29:52 +0200

    Woorden generator toegevoegd #1

--- Galgje.java
+++ Galgje.java
@@ -1,10 +1,20 @@
 package DG2;
 
+import java.util.Random;
+
 public class Galgje {
 
+	public static String randomWord() {
+		Random rn = new Random();
+		int randomnumber = rn.nextInt(30);
+		String[] woorden = {"metaal","klimaat","stuurrad","draperen","tiener","opereren","papier","gasfles","leesteken","ongedeerd",
+				"bufferen","regenmeter","kronkelen","driewieler","terras","dagloon","uranium","domein","artiest","gebaren",
+				"inkoken","meedoen","evenzo","vertellen","diagram","schedel","deurbel","hindoe","provisor","trottoir"};
+		return woorden[randomnumber];
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
