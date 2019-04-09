7af21d8a6ca98549201063bee04644904d16c16e
JeffreyMeijer <123974@student.drenthecollege.nl>
2019-04-09 11:40:27 +0200

    Galgje.java upgedate #5

--- Galgje.java
+++ Galgje.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Galgje {
 
+	public static char[] empty = new char[20];
 	// kiest één van de 30 woorden in de array.
 	public static String randomWord() {
 		Random rn = new Random();
@@ -22,23 +23,41 @@ public class Galgje {
 		return characters;
 	}
 	
-	public static void letterCheck(char[] characters) {
+	public static void letterCheck(char[] characters,boolean scannen) {
 		Scanner scanner = new Scanner(System.in);
-		char letter = scanner.next().charAt(0);
-		for(int i = 0; i < characters.length; i++) {
-			System.out.println(characters[i]);
-			if(characters[i] == letter) {
-				//Als de letter in de array zit doe .....
+		int kansen = 5;
+		while(scannen) {
+			char letter = scanner.next().charAt(0);
+			boolean fout = true;
+			
+			for(int i = 0; i < characters.length; i++) {
+				//System.out.println(characters[i]);
+				if(characters[i] == letter) {
+					fout = false;
+					empty[i] = characters[i];
+					//Als de letter in de array zit doe ...
+				}
 			}
+			
+			
+			System.out.println(empty);
+			System.out.println(characters);
+			
+			if (fout == true && kansen != 0) {
+                kansen--;
+                System.out.println("U hebt nog: " + kansen + " kansen.");
+                fout = false;
+            }
+			if (kansen == 0) {
+				System.out.println("hang jezelf");
+	            scannen = false;
+	        }
 		}
-		
 	}
 	
 	public static void main(String[] args) {
 		String woord = randomWord();
 		char[] characters = charArray(woord);
-		while(true) {
-		letterCheck(characters);
-		}
+		letterCheck(characters, true);
 	}
 }
